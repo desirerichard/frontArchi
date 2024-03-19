24a0f81cc31ff05d84c1b107fbeadd95fa5e0e5c
Désiré <github.freebie042@passmail.net>
2024-03-19 15:23:17 +0100

    feat: basic style pour la carte produit

--- src/composants/Carte.jsx
+++ src/composants/Carte.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
 export default function Carte(props) {
+  const produit = {
+    display: "flex",
+    gap: "10rem",
+    justifyContent: "space-between",
+    background: "var(--color-third)",
+    padding: "2rem",
+    borderRadius: "2rem"
+  };
   return (
-    <div key={props.id}>
+    <article style={produit}>
       <ul>
         <li>
           {props.nom} - {props.prix}
@@ -10,6 +18,6 @@ export default function Carte(props) {
         <li>{props.description}</li>
       </ul>
       <img src={props.image} />
-    </div>
+    </article>
   );
 }
